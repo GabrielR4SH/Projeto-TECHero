d9b19a30335f0f2252934e55ce4fcb5bc1373b1c
Gabriel Santana <59832080+GabrielR4SH@users.noreply.github.com>
2021-08-26 16:56:39 -0300

    Frist-Commit, Estrutura Pronta

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Theme Made By www.w3schools.com -->
+  <title>Helius Tech</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="style/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  
+  <style>
+  
+  </style>
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+
+
+<!--NavBar-->
+<nav class="navbar navbar-default navbar-fixed-top" sty>
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#myPage" style="font-size:35px;">TECHero</a>
+    </div>
+
+
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#about">SOBRE</a></li>
+        <li><a href="#services">SERVIÇOS</a></li>
+        <li><a href="#pricing">PREÇOS</a></li>
+        <li><a href="#contact">CONTATO</a></li>
+      </ul>
+    </div>
+</nav>
+<div id="about">
+
+
+<br> 
+<br>  
+<br>
+    <div id="logo">
+<img src="img/tec.png" alt="">
+</div>
+
+<div id="center-text">
+<h1 class="txt-header">TECHero</h1> 
+</div>
+<br>
+
+<div class="txt-abbout">
+
+<p class="p-abbout">Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum rem ipsa tempora commodi at sunt delectus id quis accusamus minus? Id magni consectetur voluptas debitis esse repellat non animi inventore?Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod veritatis amet provident tenetur! Dicta cupiditate debitis, rem nam consectetur laborum aperiam veniam, fuga rerum delectus blanditiis harum incidunt assumenda non! consectetur adipisicing elit. Quibusdam ducimus at, quo fuga excepturi nobis voluptas soluta quidem! Labore fugiat, debitis possimus ipsam repellat alias nostrum impedit porro aut dolorum.</p>
+<br>
+
+</div>
+
+</div>
+
+</div>
+
+    <br><br>
+
+<!-- Services Page-->
+<hr>
+<div id="services">
+<h1 class="aboutInc">Software Development</h1>
+
+<div class="space-service">
+<div class="container">
+  <br>
+
+  <div class="cen">
+<div class="one-card">
+
+<div class="card">
+  <img class="card-img-top" src="img/tec.png" alt="Card image cap">
+  <div class="card-body">
+      <br>
+    <h5 class="card-title">Software</h5>
+    <br>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+
+</div>
+<div class="two-card">
+<div class="card">
+  <img class="card-img-top" src="img/tec.png" alt="Card image cap">
+  <div class="card-body">
+      <br>
+    <h5 class="card-title">Web</h5>
+    <br>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+
+</div>
+
+<div class="three-card">
+
+<div class="card">
+  <img class="card-img-top" src="img/tec.png" alt="Card image cap">
+  <div class="card-body">
+      <br>
+    <h5 class="card-title">Mobile</h5>
+    <br>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+</div>
+
+<br> <br> <br>
+<div class="tecnologys">
+    <br>
+    <h1 style="color:white; text-align:center; font-family:lato">Nossas Tecnologias:</h1>
+    
+<div class="tecs">
+    
+
+    <img src="https://img.icons8.com/color/48/000000/html-5--v1.png"/>
+    <img src="https://img.icons8.com/color/48/000000/css3.png"/>
+<img src="https://img.icons8.com/color/48/000000/javascript--v1.png"/>
+<img src="https://img.icons8.com/color/48/000000/react-native.png"/>
+<img src="https://img.icons8.com/ios-filled/50/000000/visual-basic.png" style="background-color:purple">
+<img src="https://img.icons8.com/color/48/000000/visual-studio.png"/>
+<img src="https://img.icons8.com/officel/80/000000/php-logo.png"/>
+</div>
+
+</div>
+
+
+</div>
+</div>
+</div>
+</div>
+
+</body>
